71de9547c650f8a6dde8c701df42df5ef7bd8dd0
SebastianM <sebmr28@gmail.com>
2024-09-26 12:11:05 -0600

    Se arregla el código

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,28 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const txtname = document.getElementById('name'); //Se cambia el nombre de las constantes por algo más legible 
+const txtblog = document.getElementById('blog'); //Se quita el # de blog 
+const txtlocation = document.getElementById('location'); //Se quita el punto de location
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+async function displayUser(username) {
+  txtname.textContent = 'cargando...';   //Esta línea se pasa al HTML para mejor lectura
+  try{
+  const response = await fetch(`${usersEndpoint}/${username}`); //Para usar await se tiene que definir la función como async
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  txtname.textContent = '${data.name}';
+  blog.textContent = '${data.blog}';
+  location.textContent = '${data.location}';
+}catch (err){
+  console.log('Error:', err);
+  txtname.textContent = `Algo salió mal: ${err}`
+}
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+   //Cambiamos esta línea dentro del try-catch para mejroar el codigo
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser("Stolinski"); //Cambio a comillas doble para no tener problemas :C
+//no veo que sirva esto pero no entiendo aaaa
